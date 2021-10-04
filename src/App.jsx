0f8502caba01b8cd06cb98e19fdf09ecd450c692
Sumon Chhetry <sumonchhetry1@gmail.com>
2021-10-04 12:10:40 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AddUser from "./components/Users/AddUser"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
